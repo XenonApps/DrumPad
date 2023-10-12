7e9774b607d8f44d6809308ad6cc2b73f1cc5a7f
Iain Caldwell <xenonbusinessapps@gmail.com>
2023-10-12 13:20:08 +0100

    Initial Commit

---
+++ DrumPad/DrumPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrumPadApp.swift
+//  DrumPad
+//
+//  Created by Iain Caldwell on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrumPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
