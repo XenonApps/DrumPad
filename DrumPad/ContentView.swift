2ade2716665c672fdc67c695c17496714c2f3b87
Iain Caldwell <xenonbusinessapps@gmail.com>
2023-10-12 13:26:28 +0100

    Initial commit

--- DrumPad/ContentView.swift
+++ DrumPad/ContentView.swift
@@ -6,16 +6,49 @@
 //
 
 import SwiftUI
+import AudioKit
+
+class DrumClass: ObservableObject {
+    let engine = AudioEngine()
+    let instrument = AppleSampler()
+    
+    @Published var playing = false
+    
+    init() {
+        engine.output = instrument
+        try? engine.start()
+    }
+    
+    func loadInstrument() {
+        do {
+            if let fileURL = Bundle.main.url(forResource: "Sounds/drumSimp", withExtension: "exs") {
+                try instrument.loadInstrument(url: fileURL)
+            } else {
+                Log("Could not find file")
+            }
+        } catch {
+            Log("Could not load instrument")
+        }
+    }
+}
 
 struct ContentView: View {
+    
+    @StateObject var conductor = DrumClass()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Color.black.opacity(0.5))
+            .aspectRatio(contentMode: .fit)
+            .gesture(DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                .onChanged { _ in
+                    if !conductor.playing {
+                        conductor.instrument.play(noteNumber: MIDINoteNumber(36), velocity: 90, channel: 0)
+                    }
+                }
+                .onEnded { _ in
+                    conductor.playing = false
+                })
     }
 }
 
